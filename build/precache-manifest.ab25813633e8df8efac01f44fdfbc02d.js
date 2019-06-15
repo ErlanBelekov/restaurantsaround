b263ba091fd652fc4dd433ef06efe57506819eec
ErlanBelekov <belekoverlanhehe@gmail.com>
2019-06-15 09:02:33 +0300

    new build

---
+++ build/precache-manifest.ab25813633e8df8efac01f44fdfbc02d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bcdaea48c1df04e80819f2f6bb3d65",
+    "url": "/restaurantsaround/index.html"
+  },
+  {
+    "revision": "da77735caf0891882c72",
+    "url": "/restaurantsaround/static/css/main.c0f55e97.chunk.css"
+  },
+  {
+    "revision": "b5147d21182882e74dca",
+    "url": "/restaurantsaround/static/js/2.b8b95700.chunk.js"
+  },
+  {
+    "revision": "da77735caf0891882c72",
+    "url": "/restaurantsaround/static/js/main.1d7593e5.chunk.js"
+  },
+  {
+    "revision": "b22c8d1eb8090b424832",
+    "url": "/restaurantsaround/static/js/runtime~main.a0adfa70.js"
+  },
+  {
+    "revision": "64dee739da48748a8e75feb778117a33",
+    "url": "/restaurantsaround/static/media/search_icon.64dee739.svg"
+  }
+]);
